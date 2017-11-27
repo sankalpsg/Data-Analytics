898068b798433d7680dc202133872d28bc780566
Sankalp Gupta <31817289+sankalpsg@users.noreply.github.com>
2017-11-27 20:27:27 +0000

    Update assignmt1.R
    Train and test

--- Assignment/assignmt1.R
+++ Assignment/assignmt1.R
@@ -1,12 +1,12 @@
 
 # Defining the path
-path<-'C:/Users/Sankalp/Documents/R/Files/'
+path<-'C:/Users/guptasa/Downloads'
 setwd(path)
 
 
 #Importing the dataset
 library(readxl)
-employee <- read_excel("~/R/Files/Employeeattrition.xlsx")
+employee <- read_excel("C:/Users/guptasa/Downloads/Employeeattrition.xlsx")
 view(employee)
 attach(employee)
 
@@ -35,11 +35,21 @@ summary(e1)
 #Check on NearZero Variance
 library(caret)
 x <-nearZeroVar(employee,saveMetrics=TRUE)
+x
+
+#Splitting the dataset into Training Set and Test Set
+install.packages('caTools')
+library(caTools)
+set.seed(111)
+split=sample.split(employee$Attrition, SplitRatio = 0.75)
+
+training_set = subset(employee, split == TRUE)
+test_set = subset(employee, split == FALSE)
 
 
 #Fitting a Tree using Rpart
 library(rpart)
-fit <- rpart(Attrition ~ .,data=employee[,c(-9,-10)])
+fit <- rpart(Attrition ~ .,data=training_set[,c(-9,-10,-27)])
 
 
 #Summarizing the parameters of the fit
@@ -50,7 +60,39 @@ printcp(fit)
 plotcp(fit)
 
 
+# we see that the best Cp value is at 0.016854
+# Root node error: 178/1103 = 0.16138
+
+# n= 1103 
+# 
+# CP nsplit rel error  xerror     xstd
+# 1 0.053371      0   1.00000 1.00000 0.068639
+# 2 0.030899      2   0.89326 0.98315 0.068169
+# 3 0.025281      4   0.83146 0.96629 0.067691
+# 4 0.019663      6   0.78090 0.93258 0.066714
+# 5 0.016854      8   0.74157 0.91573 0.066214
+# 6 0.010000     10   0.70787 0.92135 0.066381
+
+#Plotting the tree
+
+library(rattle)
+drawTreeNodes(fit,cex=.8,pch=11,size=4*.8, col=NULL,nodeinfo=TRUE,
+              units="",cases="obs",digits=getOption("digits"),decimals=2,print.levels=TRUE,
+              new=TRUE)
+
+
+
 #Predicting the probabilities of the fit
 predvalues<-predict(fit)
+
+
+#Predicting the probabilities of the test_set
+test <- predict(fit,test_set,type='class')
+summary(test)
+
+summary(test_set$Attrition)
+
+
+
 
 
